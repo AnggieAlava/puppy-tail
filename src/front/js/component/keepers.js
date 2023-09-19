0a2c58c9449bd82680324897020911c4a3f74945
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-09-19 06:08:53 +0000

    Solucionada el bug de los keepersToSHow

--- src/front/js/component/keepers.js
+++ src/front/js/component/keepers.js
@@ -5,32 +5,42 @@ import { Link } from "react-router-dom";
 
 const Keepers = () => {
   const { store, actions } = useContext(Context);
+  const limit = 5;
   useEffect(() => {
-    actions.keepersToShow(2);
+    actions.keepersToShow(limit);
   }, []);
+
   return (
-    <div className="row row-cols-1 row-cols-sm-3 g-4">
-      {store.keepersToShow.map((keeper, index) => (
-        <div className="col p-4" key={index}>
-          <div className="card">
-            <img src={Perfil} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{keeper.first_name}</h5>
-              <p className="card-text">{keeper.description}</p>
-            </div>
-            <button
-              type="button"
-              className="btn btn-link"
-              style={{ textDecoration: "none" }}>
-              <Link
-                to="/profile"
-                style={{ color: "black", textDecoration: "none" }}>
-                See more...
-              </Link>
-            </button>
-          </div>
+    <div>
+      {store.keepersToShow.length === 0 ? (
+        <div className="alert alert-warning" role="alert">
+          Keepers not found
         </div>
-      ))}
+      ) : (
+        <div className="row row-cols-1 row-cols-sm-3 g-4">
+          {store.keepersToShow.map((keeper, index) => (
+            <div className="col p-4" key={index}>
+              <div className="card">
+                <img src={Perfil} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{keeper.first_name}</h5>
+                  <p className="card-text">{keeper.description}</p>
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-link"
+                  style={{ textDecoration: "none" }}>
+                  <Link
+                    to="/profile"
+                    style={{ color: "black", textDecoration: "none" }}>
+                    See more...
+                  </Link>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };

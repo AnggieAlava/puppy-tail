a436e4f03eee4a3fceb63b1d39de5177389720f3
Francisco Salvatierra <103911827+FranSalvatierra16@users.noreply.github.com>
2023-10-09 01:52:36 +0000

    page not found

--- src/front/js/pages/notFound.js
+++ src/front/js/pages/notFound.js
@@ -5,12 +5,18 @@ const NotFound = () => {
     const navigate = useNavigate();
 
     const handleReturnHome = () => {
-        navigate("/login"); // Redirige al home
+        navigate("/login");
     };
 
     return (
         <div className="not-found">
-            <div className="error-flag">Error</div>
+            <div className="image-container">
+                <img
+                    src="https://th.bing.com/th/id/OIP.Gfzn0VbWObOFQilT1aCtSQHaFE?pid=ImgDet&w=883&h=605&rs=1"
+                    alt="Error 404"
+                    className="error-image"
+                />
+            </div>
             <div className="content">
                 <h1>404 - Not Found</h1>
                 <p>La página que estás buscando no se encuentra.</p>

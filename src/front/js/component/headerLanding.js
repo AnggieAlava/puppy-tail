fef8b7df3baab02a84a9bf9ac9aeb4bf7e744a14
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-10-09 15:49:22 +0000

    Landin mejorado

--- src/front/js/component/headerLanding.js
+++ src/front/js/component/headerLanding.js
@@ -3,17 +3,17 @@ import "../../styles/headerLanding.css"
 import { Link } from "react-router-dom";
 
 const HeaderLanding = () => {
-    return (
-        <div className="container-fluid p-0 d-flex align-items-center justify-content-center main-card">
-  <div className="card bg-transparent card-header">
-    <div className="card-body text-center">
-      <h1 className="card-title landing-title">Cuidamos tus mascotas mientras estás de vacaciones</h1>
-      <p className="card-text landing-p">Encuentra personas cariñosas, verificadas y con reseñas para cuidar a tus miembros peludos de la familia mientras estás fuera de casa.</p>
-      <Link to="/signup" className="btn-header-landing">Regístrate gratis</Link>
+  return (
+    <div className="container-fluid p-0 d-flex align-items-center justify-content-center main-card">
+      <div className="card card-header">
+        <div className="card-body text-center">
+          <h1 className="card-title landing-title"><span className='cuidamos-text'>Cuidamos</span> tus <span className="mascotas-text">mascotas</span> mientras estás de vacaciones</h1>
+          {/* <p className="card-text landing-p">Encuentra personas cariñosas, verificadas y con reseñas para cuidar a tus miembros peludos de la familia mientras estás fuera de casa.</p> */}
+        </div>
+      </div>
+      <Link to="/signup" className="btn-header-landing mt-5">Regístrate gratis</Link>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default HeaderLanding;
--- src/front/js/component/ourServices.js
+++ src/front/js/component/ourServices.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState }  from 'react';
+import React, { useContext, useState } from 'react';
 import dogWalk from "../../img/dogWalk.png"
 import dogShower from "../../img/shower.png"
 import dogParty from "../../img/dogParty.png"
@@ -7,13 +7,11 @@ const OurServices = () => {
     const [showModal, setShowModal] = useState(false);
     const [modalContent, setModalContent] = useState('');
 
-    // Función para abrir el modal con el contenido deseado
     const openModal = (content) => {
         setModalContent(content);
         setShowModal(true);
     };
 
-    // Función para cerrar el modal
     const closeModal = () => {
         setShowModal(false);
     };
@@ -21,73 +19,73 @@ const OurServices = () => {
         <div className="container mt-5">
             <h2 className="text-center mt-2">Nuestros servicios</h2>
             <div className="row">
-                <div className="col d-flex justify-content-center mt-2" onClick={() => openModal('Detalles de como organizamos las fiestas')}>
+                <div className="col d-flex justify-content-center mt-4 col-steps" onClick={() => openModal('Detalles de como organizamos las fiestas')}>
                     <div className="card services-uno" style={{ width: '18rem' }}>
                         <div className="text-center">
-                            {/* <img src={dogWalk} className="card-img-top img-services-land" alt="..." /> */}
+
                         </div>
-                        <div className="card-body">
-                            <h5 className="card-title text-center">Organizadores de fiesta</h5>
-                            {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        <div className="card-body p-0">
+                            <h5 className="card-title text-center title-card-steps">Organizadores de fiesta</h5>
+
                         </div>
                     </div>
                 </div>
-                <div className="col d-flex justify-content-center mt-2"onClick={() => openModal('Detalles de los paseos de perro')}>
+                <div className="col d-flex justify-content-center mt-4 col-steps" onClick={() => openModal('Detalles de los paseos de perro')}>
                     <div className="card services-dos" style={{ width: '18rem' }}>
                         <div className="text-center">
-                            {/* <img src={dogShower} className="card-img-top img-services-land" alt="..." /> */}
+
                         </div>
-                        <div className="card-body">
-                            <h5 className="card-title text-center">Paseo de perros</h5>
-                            {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        <div className="card-body p-0">
+                            <h5 className="card-title text-center title-card-steps">Paseo de perros</h5>
+
                         </div>
                     </div>
                 </div>
-                <div className="col d-flex justify-content-center mt-2"onClick={() => openModal('Detalles de las visitas a domicilio')}>
+                <div className="col d-flex justify-content-center mt-4 col-steps" onClick={() => openModal('Detalles de las visitas a domicilio')}>
                     <div className="card services-tres" style={{ width: '18rem' }}>
                         <div className="text-center">
-                            {/* <img src={dogParty} className="card-img-top img-services-land" alt="..." /> */}
+
                         </div>
-                        <div className="card-body">
-                            <h5 className="card-title text-center">Visitas a domicilio</h5>
-                            {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        <div className="card-body p-0">
+                            <h5 className="card-title text-center title-card-steps">Visitas a domicilio</h5>
+
                         </div>
                     </div>
                 </div>
-                <div className="col d-flex justify-content-center mt-2"onClick={() => openModal('Detalles de la Guardería de día')}>
+                <div className="col d-flex justify-content-center mt-4 col-steps" onClick={() => openModal('Detalles de la Guardería de día')}>
                     <div className="card services-cuatro" style={{ width: '18rem' }}>
                         <div className="text-center">
-                            {/* <img src={dogParty} className="card-img-top img-services-land" alt="..." /> */}
+
                         </div>
-                        <div className="card-body">
-                            <h5 className="card-title text-center">Guardería de día</h5>
-                            {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        <div className="card-body p-0">
+                            <h5 className="card-title text-center title-card-steps">Guardería de día</h5>
+
                         </div>
                     </div>
                 </div>
             </div>
             {showModal && (
-    <div className="modal show" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
-        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">Detalles del Servicio</h5>
-                    <button type="button" className="close" onClick={closeModal}>
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                <div className="modal show" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
+                    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Detalles del Servicio</h5>
+                                {/* <button type="button" className="close" onClick={closeModal}>
+                                    <span aria-hidden="true">&times;</span>
+                                </button> */}
+                            </div>
+                            <div className="modal-body">
+                                <p>{modalContent}</p>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" onClick={closeModal}>
+                                    Cerrar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="modal-body">
-                    <p>{modalContent}</p>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" onClick={closeModal}>
-                        Cerrar
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-)}
+            )}
         </div>
     );
 };

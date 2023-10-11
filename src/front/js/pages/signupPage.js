3136bb93cbb29f4664b39e812abb4b36c27a8eee
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-10-11 00:25:50 +0000

    Signup page terminada

--- src/front/js/pages/signupPage.js
+++ src/front/js/pages/signupPage.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import HeaderSignupPage from '../component/headerSignupPage';
 import { SignupKeeper } from './signupKeeper';
-import ServicesList from '../component/servicesList';
 import Steps from "../component/steps"
 import FeatureServices from '../component/featureServices';
 import "../../styles/signupPage.css"
+import FooterSignupPage from '../component/footerSignupPage';
 
 const SignupPage = () => {
     return (
         <div className="signup-page">
             <HeaderSignupPage />
-            {/* <ServicesList /> */}
             <FeatureServices />
             <Steps />
             <SignupKeeper />
+            <FooterSignupPage />
         </div>
     )
 }

11c9394fcadede9fd6722f693345fbbfcfac42a5
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-10-17 02:16:18 +0000

    Checkout y modal de checkout funcional y lista interfaz responsiva

--- src/front/js/component/checkout.js
+++ src/front/js/component/checkout.js
@@ -13,9 +13,9 @@ const Checkout = () => {
     const { createPayment } = actions;
     const [showModal, setShowModal] = useState(false);
     const [modalContent, setModalContent] = useState('');
-    const phoneNumberFromAPI = store.currentUser.phone_number;
-    const phoneNumberWithoutPlus = phoneNumberFromAPI.substring(1);
-    const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumberWithoutPlus}`;
+    const phoneNumber = store.currentUser.phone_number;
+    const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}`;
+
 
 
     const openModal = (content) => {
@@ -76,25 +76,32 @@ const Checkout = () => {
 
     return (
         <PayPalScriptProvider options={{ "client-id": CLIENT_ID }}>
-            <div className="container-fluid">
-                <div className="container d-flex justify-content-center align-items-center flex-column">
-                    <h4 className="p-5">Por favor confirma la informacion de tu reserva</h4>
+            <div className="container-fluid wrap-checkout py-5">
+                <div className="container d-flex justify-content-center align-items-center flex-column bg-checkout p-3 w-75">
+                    <h5 className="p-3 text-center">Por favor confirma <br />la informacion de tu reserva</h5>
                     <div className="product-img">
                         <img
                             src={store.currentUser.profile_pic}
                             alt=""
-                            height="300"
-                            width="300" />
+                            height="200"
+                            width="200"
+                            className="checkout-img p-2" />
                     </div>
                     <div className="product-info">
                         <div className="product-text">
-                            <h6>{store.currentUser.first_name} {store.currentUser.last_name}</h6>
-                            <h6>{store.dates.start_date} {store.dates.end_date} {store.dates.start_hour} {store.dates.end_hour}</h6>
-                            <h6>{store.dates.service}</h6>
-                            <h6>{store.currentUser.location}</h6>
+                            <h6 className="m-2"><i className="fa-solid fa-user checkout-icon"></i> Cuidador: {store.currentUser.first_name} {store.currentUser.last_name}</h6>
+                            <hr/>
+                            <h6 className="m-2"><i className="fa-regular fa-calendar checkout-icon"></i> Desde: {store.dates.start_date} / {store.dates.start_hour} </h6>
+                            <hr/>
+                            <h6 className="m-2"><i className="fa-solid fa-calendar checkout-icon"></i> Hasta: {store.dates.end_date} / {store.dates.end_hour}</h6>
+                            <hr/>
+                            <h6 className="m-2"><i className="fa-solid fa-shop checkout-icon"></i> Reserva: {store.dates.service}</h6>
+                            <hr/>
+                            <h6 className="m-2"><i className="fa-solid fa-location-dot checkout-icon"></i> Ubicacion: {store.currentUser.location}</h6>
+                            <hr/>
                         </div>
                         <div className="product-price-btn">
-                            <h6>{store.currentUser.hourly_pay}</h6>
+                            <h6 className="m-2"><i className="fa-brands fa-paypal checkout-icon"></i> Precio: {store.currentUser.hourly_pay}</h6>
                         </div>
                     </div>
                     <PayPalButtons
@@ -103,40 +110,45 @@ const Checkout = () => {
                         createOrder={createOrder}
                         onApprove={onApprove}
                     />
-                    <button className='btn btn-danger' onClick={handleGoBack}>Cancelar</button>
+                    <button className='btn btn-orange' onClick={handleGoBack}>Cancelar</button>
                 </div>
-
-
             </div>{showModal && (
                 <div className="container-fluid">
-                    <div className="container d-flex justify-content-center align-items-center flex-column">
+                    <div className="container d-flex justify-content-center align-items-center flex-column ">
                         <div className="modal show" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
                             <div className="modal-dialog modal-fullscreen" role="document">
-                                <div className="modal-content">
+                                <div className="modal-content modal-checkout">
                                     <div className="modal-header">
-                                        <h6 className="modal-title"><i className="fa-solid fa-check"></i>Tu reserva fue exitosa</h6>
+                                        <h6 className="modal-title"><i className="fa-solid fa-check exit-checkout"></i>Tu reserva fue exitosa</h6>
                                     </div>
                                     <div className="modal-body">
                                         <h6>Haz clic en el siguiente link para contactarte con tu cuidador</h6>
-                                        <a href={`mailto:${store.currentUser.email}`} target="_blank">
-                                            <button><i className="fa-solid fa-envelope"></i></button>
-                                        </a>
-                                        <a href={whatsappLink} target="_blank">
-                                            <button><i className="fa-brands fa-whatsapp"></i></button>
-                                        </a>
-                                        <h6><i className="fa-solid fa-user"></i> {store.currentUser.first_name} {store.currentUser.last_name}</h6>
-                                        <h6><i className="fa-solid fa-calendar-days"></i> {store.dates.start_date} {store.dates.end_date} {store.dates.start_hour} {store.dates.end_hour}</h6>
-                                        <h6><i className="fa-solid fa-earth-americas"></i> {store.currentUser.location}</h6>
-                                        <h6><i className="fa-solid fa-handshake"></i> {store.dates.service}</h6>
-                                        <h6><i className="fa-solid fa-dollar-sign"></i> {store.currentUser.hourly_pay}</h6>
+                                        <div className="d-flex justify-content-center align-items-center me-4" >
+                                            <a href={`mailto:${store.currentUser.email}`} target="_blank">
+                                                <button><i className="fa-solid fa-envelope"></i></button>
+                                            </a>
+                                            <a href={whatsappLink} target="_blank">
+                                                <button><i className="fa-brands fa-whatsapp"></i></button>
+                                            </a>
+                                        </div>
+
+                                        <h6><i className="fa-solid fa-user checkout-icon"></i> {store.currentUser.first_name} {store.currentUser.last_name}</h6>
+                                        <hr/>
+                                        <h6><i className="fa-solid fa-calendar-days checkout-icon"></i> {store.dates.start_date} {store.dates.end_date} {store.dates.start_hour} {store.dates.end_hour}</h6>
+                                        <hr/>
+                                        <h6><i className="fa-solid fa-earth-americas checkout-icon"></i> {store.currentUser.location}</h6>
+                                        <hr/>
+                                        <h6><i className="fa-solid fa-handshake checkout-icon"></i> {store.dates.service}</h6>
+                                        <hr/>
+                                        <h6><i className="fa-solid fa-dollar-sign checkout-icon"></i> {store.currentUser.hourly_pay}</h6>
                                     </div>
                                     <div className="modal-body">
-                                        <Link to="/home"><button>Home</button></Link>
+                                    <button className="btn-green me-3  d-print-none"onClick={() => window.print()}>Imprimir</button>
                                         <Link to={"/profile/" + store.userInfo.user_type + "/" + store.userInfo.userId}>
-                                            <button>Ir a mi perfil</button></Link>
+                                            <button className="btn-green d-print-none">Ir a mi perfil</button></Link>
                                     </div>
                                     <div className="modal-footer">
-                                        <Link to={"/profile/" + store.userInfo.user_type + "/" + store.userInfo.userId}><button type="button" className="btn btn-danger" onClick={closeModal}>
+                                        <Link to="/home"><button type="button" className="btn btn-orange d-print-none" onClick={closeModal}>
                                             Cerrar
                                         </button></Link>
                                     </div>
@@ -150,5 +162,4 @@ const Checkout = () => {
 
     );
 }
-
 export default Checkout;

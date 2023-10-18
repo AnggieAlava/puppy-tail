f7527a4c6dc5b01a5e2da3f4afa1d46c61a2e41b
Francisco Salvatierra <103911827+FranSalvatierra16@users.noreply.github.com>
2023-10-18 00:37:00 +0000

    filtros y home

--- src/front/js/component/keepers.js
+++ src/front/js/component/keepers.js
@@ -1,8 +1,6 @@
-import { Context } from "../store/appContext";
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import "../../styles/keepers.css";
 import avatar from "../../img/avatar.jpg";
-import { FilterLocation } from "../component/filterLocation";
 import moment from "moment";
 import { Link } from "react-router-dom";
 
@@ -18,8 +16,6 @@ const Keepers = () => {
   const [showFilters, setShowFilters] = useState(false);
 
   const URL = (process.env.BACKEND_URL + '/api/keeper');
-  const [selectedLocation, setSelectedLocation] = useState(null);
-
 
   const showData = async () => {
     try {
@@ -28,128 +24,159 @@ const Keepers = () => {
         throw new Error('No se pudo obtener la información');
       }
 
-      const data = await response.json(); // Parsear la respuesta a JSON
-      console.log(data);
+      const data = await response.json();
       setKeepers(data);
       setDataLoaded(true);
     } catch (error) {
       console.error('Error en la obtención de datos:', error);
     }
   };
+
   useEffect(() => {
     showData();
   }, []);
+
   const handleLocationChange = (location) => {
     setFilters({ ...filters, location });
   };
+
   const handleFilterChange = (name, value) => {
     setFilters({ ...filters, [name]: value });
   };
+
   function imgErrorHandler(e) {
     e.target.src = avatar;
   }
-  const filteredKeepers = dataLoaded
 
-    ? keepers.filter((keeper) => {
-      return (
-        keeper.first_name.toLowerCase().includes(filters.first_name.toLowerCase()) &&
-        (filters.services === "" || keeper.services.includes(filters.services)) && // Filtrar si está seleccionado un servicio
-        calculateExperienceInYears(keeper.experience) >= filters.experience &&
-        keeper.location.toLowerCase().includes(filters.location.toLowerCase())
-      );
-    })
-    : [];
+  function filterKeepers() {
+    return keepers.filter((keeper) => {
+      const nameMatches = keeper.first_name.toLowerCase().includes(filters.first_name.toLowerCase());
+      const serviceMatches = filters.services === "" || keeper.services.includes(filters.services);
+      const experienceMatches = filters.experience === "" || calculateExperienceInYears(keeper.experience) >= parseInt(filters.experience);
+      const locationMatches = keeper.location.toLowerCase().includes(filters.location.toLowerCase());
+
+      return nameMatches && serviceMatches && experienceMatches && locationMatches;
+    });
+  }
 
   function calculateExperienceInYears(experienceStartDate) {
     const startDate = moment(experienceStartDate);
-    const endDate = moment(); // Fecha actual
+    const endDate = moment();
     const yearsOfExperience = endDate.diff(startDate, "years");
     return yearsOfExperience;
   }
+
   const toggleFilters = () => {
     setShowFilters(!showFilters);
-    if (!showFilters) {
-      setFilters({
-        first_name: "",
-        services: "",
-        experience: "",
-        location: ""
-      });
-    }
   };
 
   return (
     <div>
-      <button onClick={toggleFilters}>
-        {showFilters ? "Ocultar Filtros" : "Mostrar Filtros"}
-      </button>
-      {showFilters && (
-
-        <div className="filter-home">
-          <input
-            type="text"
-            name="first_name"
-            placeholder="Nombre del cuidador"
-            className="text-black"
-            onChange={(e) => handleFilterChange("first_name", e.target.value)}
-          />
-
-          <div className="select-container">
-            <select
-              defaultValue="0"
-              name="services"
-              onChange={(e) => handleFilterChange("services", e.target.value)}
-            >
-              <option value="0" disabled>
-                Tipo de trabajo
-              </option>
-              <option value="Cuidar mascotas">Cuidar mascotas</option>
-              <option value="Pasear mascotas">Pasear mascotas</option>
-              <option value="Organizar fiesta">Organizar fiesta</option>
-            </select>
-          </div>
-          <input
-            type="text"
-            name="experience"
-            placeholder="Experiencia"
-            onChange={(e) => handleFilterChange("experience", e.target.value)}
-          />
-          <FilterLocation onLocationChange={handleLocationChange} />
+      {/* <div className="filter-container">
+        <button className="show-filters-button" onClick={toggleFilters}>
+          {showFilters ? "Ocultar Filtros" : "Mostrar Filtros"}
+        </button>
+        {showFilters  && ( */}
+      <div className="filter-home" >
+        <input
+          type="text"
+          name="first_name"
+          placeholder="Nombre del cuidador"
+          className="text-black"
+          value={filters.first_name}
+          onChange={(e) => handleFilterChange("first_name", e.target.value)}
+        />
+        <div className="select-container">
+          <select
+            defaultValue="0"
+            name="services"
+            value={filters.services}
+            onChange={(e) => handleFilterChange("services", e.target.value)}
+          >
+            <option value="0" disabled>
+              Tipo de trabajo
+            </option>
+            <option value="Cuidar mascotas">Cuidar mascotas</option>
+            <option value="Pasear mascotas">Pasear mascotas</option>
+            <option value="Organizar fiesta">Organizar fiesta</option>
+          </select>
         </div>
-      )}
-      <div className="row row-cols-1 row-cols-sm-3 g-4 card-wrap">
+        <input
+          type="text"
+          name="experience"
+          placeholder="Experiencia"
+          value={filters.experience}
+          onChange={(e) => handleFilterChange("experience", e.target.value)}
+        />
+        <input
+          type="text"
+          name="location"
+          placeholder="Ubicación"
+          value={filters.location}
+          onChange={(e) => handleFilterChange("location", e.target.value)}
+        />
 
-        {filteredKeepers.map((keeper, index) => (
+
+        <button
+          className="clear-filters-button"
+          onClick={() => {
+            setFilters({
+              first_name: "",
+              services: "",
+              experience: "",
+              location: "",
+            });
+          }}
+        >
+          Borrar filtros
+        </button>
+      </div>
+
+      {/* )}
+      </div> */}
+
+      <div className="row row-cols-1 row-cols-sm-3 g-4 card-wrap">
+        {filterKeepers().map((keeper, index) => (
           <div className="col p-4" key={index}>
-            <Link
-              to={"/profile/keeper/" + keeper.id}
-              onClick={() =>
-                localStorage.setItem("keeper", JSON.stringify(keeper))
-              }
-              style={{ textDecoration: "none", color: "inherit" }}>
-              <div className="card">
-                <img
-                  onError={imgErrorHandler}
-                  src={keeper.profile_pic}
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    {keeper.first_name} {keeper.last_name}
-                  </h5>
-                  <h6 className="card-text">{keeper.location}</h6>
-                  <h6 className="card-text">{keeper.services}</h6>
-                  <p className="card-text">Años de experiencia: {calculateExperienceInYears(keeper.experience)}</p>
+            <div className="card">
+              <div className="row no-gutters">
+                <div className="col-md-4">
+                  <img
+                    onError={imgErrorHandler}
+                    src={keeper.profile_pic}
+                    className="card-img"
+                    alt="..."
+                  />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {keeper.first_name} {keeper.last_name}
+                    </h5>
+
+                    <h7 className="card-text">{keeper.location}</h7>
+                    <p className="card-text">Años de experiencia: {calculateExperienceInYears(keeper.experience)}</p>
+                    <p className="card-text">Tarifa: {keeper.hourly_pay} por hora</p>
+                    <Link
+                      to={`/profile/keeper/${keeper.id}`}
+                      style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                      <button className="btn-primary"
+                        onClick={() =>
+                          localStorage.setItem("keeper", JSON.stringify(keeper))
+                        }
+                      >
+                        Reservar
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </Link>
+            </div>
           </div>
         ))}
 
       </div>
-
-
     </div>
   );
 };

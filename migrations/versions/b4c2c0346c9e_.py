5aef1112e1c460d5f3e4eb6955197e9cb3f9098d
Anggie Alava <anggie.alava.12@gmail.com>
2023-10-26 22:28:33 -0500

    Rama actualizada con el docker y funcional

--- migrations/versions/b4c2c0346c9e_.py
+++ migrations/versions/b4c2c0346c9e_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 1677728422b0
+Revision ID: b4c2c0346c9e
 Revises: 
-Create Date: 2023-10-26 19:23:50.232054
+Create Date: 2023-10-26 22:26:28.710873
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '1677728422b0'
+revision = 'b4c2c0346c9e'
 down_revision = None
 branch_labels = None
 depends_on = None
